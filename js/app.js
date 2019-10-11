cea80bdcecf64c1f443b4922fba3770d20b00fe6
rjonathan87 <rjonathan87@gmail.com>
2019-10-11 11:47:40 -0500

    practicando async await

--- js/app.js
+++ js/app.js
@@ -4,27 +4,42 @@ const app = new Vue({
         datos: []
     },
     methods: {
-        getData() {
-            var that = this;
-            axios.get('https://localhost:5001/api/CodigoPostal')
-                .then(function (response) {
-                    // console.log(response.data);
-                    that.datos = response.data;
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
+        async getData() {
+            // var that = this;
+            // axios.get('https://localhost:5001/api/CodigoPostal')
+            //     .then(function (response) {
+            //         // console.log(response.data);
+            //         that.datos = response.data;
+            //     })
+            //     .catch(function (error) {
+            //         console.log(error);
+            //     });
+            // await fetch('https://localhost:5001/api/CodigoPostal')
+            //     .then(res => {
+            //         return res.json()
+            //     })
+            //     .then(data => {
+            //         this.datos = data;
+            //     })
+            //     .catch(err => {
+            //         console.log(err);
+            //     })
+            const url = `https://localhost:5001/api/CodigoPostal`
+            const res = await fetch(url)
+            const json = await res.json()
+            if(res.status !== 200)
+                throw Error('Algo malo ha sucedido!!!')
+            else
+                this.datos = json;
         }
     },
-    created: function () {
-        // var that = this;
-        // axios.get('https://localhost:5001/api/CodigoPostal')
-        //     .then(function (response) {
-        //         that.datos = response.data;
-        //     })
-        //     .catch(function (error) {
-        //         console.log('Error: ' + error);
-        //     });
-        this.getData();
+    created: async function () {
+        try {
+            await this.getData();
+        } catch (error) {
+            console.log(`Error: ${error}`);
+            
+        }
+        
     }
 })
